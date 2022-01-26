d91df9a720ed9d7e1ea51876ac300128580e6951
Willbbik <minsoo651@naver.com>
2022-01-26 23:44:36 +0900

    baekjoon 14916

--- algorithm/src/algorithm/Main.java
+++ algorithm/src/algorithm/Main.java
@@ -2,43 +2,44 @@ package algorithm;
 
 
 import java.io.*;
-import java.math.BigInteger;
 import java.util.Scanner;
 
 
-public class Main {			
-
-	static BigInteger[] dp;
-	
+public class Main {					
 	public static void main(String[] args) throws IOException {
-								
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();		
-		
-		if(n == 0) {
-			System.out.println(0);			
-		}else if(n == 1) {
-			System.out.println(1);			
-		}else {
-			dp = new BigInteger[n + 1];
-			
-			dp[0] = BigInteger.ZERO;
-			dp[1] = BigInteger.ONE;		
-			
-			System.out.println(cal(n));
-		}
-				
-	}
-	
-	
-	static BigInteger cal(int N) {
-		
-		if(dp[N] == null) {		
-			dp[N] = cal(N - 1).add(cal(N - 2));			
-		}
-		
-		return dp[N];		
-	}
 
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int cnt = 0;
+		
+		
+		if(n % 5 == 0) {
+			System.out.println(n/5);			
+		}else {
+					
+			while(true) {				
+				
+				if(n < 0) {
+					System.out.println(-1);
+					break;
+				}
+				
+				if(n == 0) {
+					System.out.println(cnt);
+					break;
+				}
+				
+				n -= 2;
+				cnt++;	
+				
+				if(n % 5 == 0) {
+					cnt += n / 5;
+					System.out.println(cnt);
+					break;
+				}		
+			}
+		}
+		
+	}	
 }
 
