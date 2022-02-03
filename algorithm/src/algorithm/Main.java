1a9540856d80b9cecb828164d7209bc8710c0cd7
Willbbik <minsoo651@naver.com>
2022-02-03 22:22:46 +0900

    baekjoon 9656

--- algorithm/src/algorithm/Main.java
+++ algorithm/src/algorithm/Main.java
@@ -6,40 +6,21 @@ import java.util.Scanner;
 
 
 public class Main {				
-	// baekjoon 2491
+
 		
 	static int[] arr;
 	
 	public static void main(String[] args) throws IOException {
 
 		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
 		
-		int n = sc.nextInt();	
-		int count = 1;	
-		int count2 = 1;
-		int max = 1;
-		arr = new int[n];
-		
-		// 값 받기
-		for(int i = 0; i < n; i++) {
-			arr[i] = sc.nextInt();
-		}		
-				
-		// 오름차순		
-		for(int i = 0; i<n-1; i++) {
-			if(arr[i] <= arr[i+1]) count++;
-			else count = 1;
-			max = Math.max(max, count);
+		if(n % 2 == 1) {
+			System.out.print("CY");
+		}else {
+			System.out.print("SK");
 		}
-		
-		// 내림차순		
-		for(int i = 0; i<n-1; i++) {
-			if(arr[i] >= arr[i+1]) count2++;
-			else count2 = 1;
-			max = Math.max(max, count2);
-		}
-		
-		System.out.println(max);
+		sc.close();
 	}	
 	
 }
