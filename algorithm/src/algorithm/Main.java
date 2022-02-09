a4cabb0d166257e671e11184ccaabc0dec9b42f6
Willbbik <minsoo651@naver.com>
2022-02-10 01:02:45 +0900

    baekjoon 16395

--- algorithm/src/algorithm/Main.java
+++ algorithm/src/algorithm/Main.java
@@ -6,21 +6,28 @@ import java.util.Scanner;
 
 
 public class Main {				
-
-		
-	static int[] arr;
+	
+	static int[][] arr;	
 	
 	public static void main(String[] args) throws IOException {
 
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
+		int k = sc.nextInt();
 		
-		if(n % 2 == 1) {
-			System.out.print("CY");
-		}else {
-			System.out.print("SK");
+		arr = new int[n+1][n+1];
+		arr[1][1] = 1;
+		
+		for(int i = 2; i <= n; i++) {
+			
+			arr[i][1] = 1;
+			for(int j = 1; j < n; j++) {
+				arr[i][j] = arr[i-1][j-1] + arr[i-1][j];
+			}
+			arr[i][n] = 1;
 		}
-		sc.close();
-	}	
-	
+		
+		System.out.println(arr[n][k]);
+	}			
+
 }
