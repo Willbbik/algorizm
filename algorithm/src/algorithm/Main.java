5811e91b8a603b0eb2f438f627469d0e5916d263
Willbbik <minsoo651@naver.com>
2022-02-23 09:42:37 +0900

    baekjoon 2751

--- algorithm/src/algorithm/Main.java
+++ algorithm/src/algorithm/Main.java
@@ -1,82 +1,33 @@
 package algorithm;
 
-
-import java.io.*;
-import java.util.Scanner;
-
-
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
 
-	static int N, M, K, X;
-	static Vector<Integer>[] v;
-	static int[] dist;
 
 	public static void main(String[] args) throws Exception {
-
+			
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		String[] sarr = br.readLine().split(" ");
-		N = Integer.parseInt(sarr[0]);
-		M = Integer.parseInt(sarr[1]);
-		K = Integer.parseInt(sarr[2]);
-		X = Integer.parseInt(sarr[3]);
-
-		v = new Vector[N + 1];
-		for (int i = 0; i <= N; i++)
-			v[i] = new Vector<>();
-
-		dist = new int[N + 1];
-		for (int i = 0; i <= N; i++)
-			dist[i] = Integer.MAX_VALUE;
-
-		for (int i = 0; i < M; i++) {
-			sarr = br.readLine().split(" ");
-			int A = Integer.parseInt(sarr[0]);
-			int B = Integer.parseInt(sarr[1]);
-			v[A].add(B);
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+		List<Integer> list = new ArrayList<Integer>();
+		
+		for(int i = 0; i < n; i++) {
+			list.add(Integer.parseInt(br.readLine()));
 		}
-
-		dijkstra();
-
-		List<Integer> list = new ArrayList<>();
-		for (int i = 1; i <= N; i++) {
-			if (dist[i] == K)
-				list.add(i);
+		
+		Collections.sort(list);
+		for(Integer i : list) {
+			sb.append(i).append("\n");
 		}
-
-		if (list.size() == 0)
-			bw.write(-1 + "\n");
-		else {
-			for (int n : list) {
-				bw.write(n + "\n");
-			}
-		}
-		bw.flush();
+		
+		System.out.println(sb);
 	}
 
-	public static void dijkstra() {
-
-		PriorityQueue<Integer> pq = new PriorityQueue<>();
-
-		dist[X] = 0;
-		pq.offer(X);
-
-		while (!pq.isEmpty()) {
-
-			int n = pq.poll();
-
-			for (int m : v[n]) {
-
-				if (dist[m] > dist[n] + 1) {
-					dist[m] = dist[n] + 1;
-					pq.offer(m);
-				}
-			}
-		}
-	}
 
 }
