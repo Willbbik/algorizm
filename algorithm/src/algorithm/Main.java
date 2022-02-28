b09ca4bc5959e9572877239b357e36391fa17c0e
Willbbik <minsoo651@naver.com>
2022-02-28 22:51:41 +0900

    baekjoon 1065

--- algorithm/src/algorithm/Main.java
+++ algorithm/src/algorithm/Main.java
@@ -2,32 +2,41 @@ package algorithm;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public class Main {
 
-
 	public static void main(String[] args) throws Exception {
 			
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));			
 		int n = Integer.parseInt(br.readLine());
-		List<Integer> list = new ArrayList<Integer>();
 		
-		for(int i = 0; i < n; i++) {
-			list.add(Integer.parseInt(br.readLine()));
-		}
-		
-		Collections.sort(list);
-		for(Integer i : list) {
-			sb.append(i).append("\n");
-		}
-		
-		System.out.println(sb);
+		System.out.println(cal(n));
+	
 	}
-
+	
+	static int cal(int num) {
+		
+		int cnt = 0;
+		
+		if(num < 100) {
+			return num;
+		}else {
+			cnt = 99;
+			if(num == 1000) {
+				num = 999;
+			}
+			
+			for(int i = 100; i <= num; i++) {
+				int hun = i / 100;
+				int ten = (i / 10) % 10;
+				int one = i % 10;
+				
+				if((hun - ten) == (ten - one)) {
+					cnt ++;
+				}
+			}					
+		}	
+		return cnt;
+	}
 
 }
