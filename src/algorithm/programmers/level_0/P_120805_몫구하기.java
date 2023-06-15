d73f0fbc268c24775f29f6ddfccdbb395ea01706
minsoo <minsoo651@naver.com>
2023-06-15 23:22:32 +0900

    120805 몫 구하기

---
+++ src/algorithm/programmers/level_0/P_120805_몫구하기.java
@@ -0,0 +1,11 @@
+package algorithm.programmers.level_0;
+
+public class P_120805_몫구하기 {
+
+    class Solution {
+        public int solution(int num1, int num2) {
+            return num1 / num2;
+        }
+    }
+
+}
