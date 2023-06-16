6aa78648d016761e92ec92f175b04f967bd0cbde
minsoo <minsoo651@naver.com>
2023-06-16 23:40:02 +0900

    P_120804_두수의곱

---
+++ src/algorithm/programmers/level_0/P_120804_두수의곱.java
@@ -0,0 +1,11 @@
+package algorithm.programmers.level_0;
+
+public class P_120804_두수의곱 {
+
+    class Solution {
+        public int solution(int num1, int num2) {
+            return num1 * num2;
+        }
+    }
+
+}
