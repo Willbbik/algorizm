1c8975dcc458c22306e2e0c41bb44f1bf8f75a4f
minsoo <minsoo651@naver.com>
2023-06-18 01:15:58 +0900

    P_120803_두수의차" "

---
+++ src/algorithm/programmers/level_0/P_120803_두수의차.java
@@ -0,0 +1,11 @@
+package algorithm.programmers.level_0;
+
+public class P_120803_두수의차 {
+
+    class Solution {
+        public int solution(int num1, int num2) {
+            return num1 - num2;
+        }
+    }
+
+}
