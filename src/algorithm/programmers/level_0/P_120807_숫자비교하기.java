96a13ee7c7849d6ce8f3e2f869db01a741085d85
minsoo <minsoo651@naver.com>
2023-06-17 19:37:42 +0900

    P_120807_숫자비교하기

---
+++ src/algorithm/programmers/level_0/P_120807_숫자비교하기.java
@@ -0,0 +1,15 @@
+package algorithm.programmers.level_0;
+
+public class P_120807_숫자비교하기 {
+
+    class Solution {
+        public int solution(int num1, int num2) {
+            if(num1 == num2){
+                return 1;
+            }
+
+            return -1;
+        }
+    }
+
+}
