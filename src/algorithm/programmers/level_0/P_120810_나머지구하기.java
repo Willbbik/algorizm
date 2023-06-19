95ab6de623ff3a46e410577ef30ff0469d3aac5a
minsoo <minsoo651@naver.com>
2023-06-19 21:20:39 +0900

    P_120810_나머지구하기

---
+++ src/algorithm/programmers/level_0/P_120810_나머지구하기.java
@@ -0,0 +1,10 @@
+package algorithm.programmers.level_0;
+
+public class P_120810_나머지구하기 {
+
+    class Solution {
+        public int solution(int num1, int num2) {
+            return num1 % num2;
+        }
+    }
+}
