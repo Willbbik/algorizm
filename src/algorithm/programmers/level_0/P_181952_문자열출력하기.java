6fc4c721dfd8f2903473643434b8faa029c399dd
minsoo <minsoo651@naver.com>
2023-06-22 23:34:53 +0900

    P_181952_문자열출력하기

---
+++ src/algorithm/programmers/level_0/P_181952_문자열출력하기.java
@@ -0,0 +1,12 @@
+package algorithm.programmers.level_0;
+
+import java.util.Scanner;
+
+public class P_181952_문자열출력하기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        System.out.println(a);
+    }
+}
