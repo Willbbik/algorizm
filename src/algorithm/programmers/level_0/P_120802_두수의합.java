8070f49f3ef0597394ce0130b1c3b5df19712240
minsoo <minsoo651@naver.com>
2023-06-23 22:32:39 +0900

    P_120802_두수의합

---
+++ src/algorithm/programmers/level_0/P_120802_두수의합.java
@@ -0,0 +1,9 @@
+package algorithm.programmers.level_0;
+
+public class P_120802_두수의합 {
+    class Solution {
+        public int solution(int num1, int num2) {
+            return num1 + num2;
+        }
+    }
+}
