a2d77ee017330a7fa9c683d3d77ecac9ce89a317
minsoo <minsoo651@naver.com>
2023-06-24 19:40:37 +0900

    P_120820_나이출력

---
+++ src/algorithm/programmers/level_0/P_120820_나이출력.java
@@ -0,0 +1,11 @@
+package algorithm.programmers.level_0;
+
+public class P_120820_나이출력 {
+    class Solution {
+        public int solution(int age) {
+            int nowYear = 2023;
+
+            return nowYear - age;
+        }
+    }
+}
