e7c70f95917bff28ba401439766525b192169063
minsoo <minsoo651@naver.com>
2023-07-03 23:02:47 +0900

    P_120898_편지

---
+++ src/algorithm/programmers/level_0/P_120898_편지.java
@@ -0,0 +1,9 @@
+package algorithm.programmers.level_0;
+
+public class P_120898_편지 {
+    class Solution {
+        public int solution(String message) {
+            return message.length() * 2;
+        }
+    }
+}
